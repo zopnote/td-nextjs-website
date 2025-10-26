dd874c5100e4facf497b5a3e25298dccb60bbe77
lenny <lennysiebert08@gmail.com>
2025-10-26 22:20:12 +0100

    make bigger hero banner

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Parallax from "@/app/start/components/parallax";
 export default function Home() {
   return (
       <>
-          <Parallax height={"45vh"} content={<TitleContainer/>} />
+          <Parallax height={"100vh"} content={<TitleContainer/>} />
           <div style={{height: "60vh"}}><ServiceMapWrapper /></div>
           <div>
               <p className={"title p-3"}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
